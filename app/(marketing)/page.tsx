ba18807ede15d11e0ac4e6269922953606ead44b
nate <qnate275@gmail.com>
2024-05-10 16:53:46 +0800

    adding icons

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -22,37 +22,39 @@ export default function Home() {
         <h1 className=" text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480] text-center">
           Learn,pratise,and master new languages with iroLingo
         </h1>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          <ClerkLoading>
+            <Loader className=" h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                signInForceRedirectUrl="/learn"
+                forceRedirectUrl="/learn"
+              >
+                <Button size="lg" variant="secondery" className=" w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                signUpForceRedirectUrl="/learn"
+                forceRedirectUrl="/learn"
+              >
+                <Button size="lg" variant="primaryOutline" className=" w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondery" className=" w-full" asChild>
+                <Link href="/learn">Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
       </div>
-      <ClerkLoading>
-        <Loader className=" h-5 w-5 text-muted-foreground animate-spin" />
-      </ClerkLoading>
-      <ClerkLoaded>
-        <SignedOut>
-          <SignUpButton
-            mode="modal"
-            signInForceRedirectUrl="/learn"
-            forceRedirectUrl="/learn"
-          >
-            <Button size="lg" variant="secondery" className=" w-full">
-              Get Started
-            </Button>
-          </SignUpButton>
-          <SignInButton
-            mode="modal"
-            signUpForceRedirectUrl="/learn"
-            forceRedirectUrl="/learn"
-          >
-            <Button size="lg" variant="primaryOutline" className=" w-full">
-              I already have an account
-            </Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <Button size="lg" variant="secondery" className=" w-full" asChild>
-            <Link href="/learn">Continue Learning</Link>
-          </Button>
-        </SignedIn>
-      </ClerkLoaded>
     </div>
   );
 }
