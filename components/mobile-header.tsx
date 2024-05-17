7c236e94a468ebede1f36d1253a061571aeda81e
nate <qnate275@gmail.com>
2024-05-17 16:40:59 +0800

    debug

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -2,7 +2,7 @@ import { MobileSiderbar } from "./mobile-siedebar";
 
 export const MobileHeader = () => {
   return (
-    <nav className="lg:hidden px-4 h-[50px] items-center bg-green-500 border-b fixed top-0 w-full z-50">
+    <nav className="lg:hidden px-4 h-[50px] items-center bg-green-500 border-b fixed top-0 w-full z-50 flex">
       <MobileSiderbar />
     </nav>
   );
